a11638b1eda10434480867dda7d9b7489fe08e86
Shrutika Mendhe <shrutika.mendhe@click2cloud.net>
2017-11-04 23:49:37 +0530

    uploading exp 5 code base

---
+++ BotCode/ForOption 4/FlightStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SimpleEchoBot.Model
+{
+    [Serializable]
+    public class FlightStatus
+    {
+        public string Flight { get; set; }
+        public string FromTerminalNo { get; set; }
+        public string FromGateNo { get; set; }
+        public string departTime { get; set; }
+    }
+}
